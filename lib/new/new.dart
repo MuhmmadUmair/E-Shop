5c81e2a236c39dc36087755575b9bec2b0149960
MuhmmadUmair <143011974+MuhmmadUmair@users.noreply.github.com>
2025-06-28 23:16:41 +0500

    Added new file

---
+++ lib/new/new.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewBranch extends StatelessWidget {
+  const NewBranch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
