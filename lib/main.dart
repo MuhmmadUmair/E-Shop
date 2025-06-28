82bbcfad1b1a0b35a82e213a069166b9aca2b2ef
MuhmmadUmair <143011974+MuhmmadUmair@users.noreply.github.com>
2025-06-28 23:25:09 +0500

    New Change

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Comment /Provider
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (ctx) => Auth()),
